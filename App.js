4b5221cd98cc4ab606a31daea3d2b86fa5a7b2b6
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-16 20:23:54 -0400

    second commit

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-//  app.get('/', (req, res) => {
-//  res.send('
-//  <html>
-// <head>
-// title>Puppies and Kittens Site</title>
-// </head>
-// <body>
-// <h1>Puppies and Kittens Galore</h1>
-// </body>
-// </html>
-//    ')
-// })
-// App.get('/puppies', (req, res) => {
-//  res.send(//Omitted for brevity)
-// })
+ app.get('/', (req, res) =>{
+     res.send(
+     <html>
+     <head>
+         <title>Puppies and Kittens Site</title>
+     </head>
+     <body>
+         <h1>Puppies and Kittens Galoe</h1>
+     </body>
+     </html>
+     )
+ });
 
 
 
 const PORT = 1337;
 App.listen(PORT, () => {
-    console.log('listening on port $[PORT]');
+    console.log('listening on port ${PORT}');
 });
